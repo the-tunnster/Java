75858ce12d16e6adc7a2a6992ff8642fcbca81a3
Tarun Krishnan <therealtarunkrishnan@gmail.com>
2021-07-19 11:16:28 +0530

    Added and implemented a class for Users.

---
+++ CSVReader/User.java
@@ -0,0 +1,35 @@
+package CSVReader;
+
+public class User {
+	
+	private String firstName;
+	private String lastName;
+	private String emailID;
+	private String phoneNumber;
+	private String city;
+
+	public User(){
+		this.firstName = "Default";
+		this.lastName = "Default";
+		this.emailID = "default@email.com";
+		this.phoneNumber = "0000000000";
+		this.city = "Default";
+	}
+
+	public User(String fN, String lN, String eID, String pN, String c){
+		this.firstName = fN;
+		this.lastName = lN;
+		this.emailID = eID;
+		this.phoneNumber = pN;
+		this.city = c;
+	}
+
+	public void DisplayUserInformation(){
+		System.out.print("First name : " + this.firstName+ ", ");
+		System.out.print("Last name : " + this.lastName + ", ");
+		System.out.print("Email ID : " + this.emailID + ", ");
+		System.out.print("Phone number : " + this.phoneNumber + ", ");
+		System.out.println("City : " + this.city + ".");
+
+	}
+}
