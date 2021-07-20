c59864c84080c34b22888cafd78405068afb6b9b
Tarun Krishnan <therealtarunkrishnan@gmail.com>
2021-07-20 18:38:25 +0530

    Inhetiance example usage added.

--- InheritanceWork/Main.java
+++ InheritanceWork/Main.java
@@ -4,6 +4,8 @@ public class Main{
 
 	public static void main(String []args){
 
+		
+		//Standard instantiation, no inheritance
 		First f = new First(1, 2, 3);
 		Second s = new Second(1, 2, 3, 4, 5, 6);
 		Third t = new Third(1, 2, 3, 7, 8, 9);
@@ -12,9 +14,15 @@ public class Main{
 		s.Display();
 		t.Display();
 
-		f.Adder();
-		s.Adder();
-		t.Adder();
+
+
+		//Inhertable instantiation
+		First f2 = new Second(1, 2, 3, 4, 5, 6);
+		First f3 = new Third(1, 2, 3, 7, 8, 9);
+
+		f.Display();
+		f2.Display();
+		f3.Display();
 		
 
 	}
